aaf10aed5e4fce90f556d175a794ebb483c36ed1
Poney <arthur.agombart@gmail.com>
2017-12-11 01:10:45 +0100

    Update test cases

--- src/dev-server.js
+++ src/dev-server.js
@@ -2,6 +2,55 @@ import angular from 'angular';
 
 import './input-file.component';
 
+function DevServerComponent($timeout) {
+  const vm = this;
+  vm.$onInit = () => {
+    vm.files = [];
+  };
+
+  vm.fileChanged = result => {
+    console.log('Hey !', result);
+    vm.files.length = 0;
+    $timeout(() => {
+      [].push.apply(vm.files, result);
+    });
+  };
+}
+
 angular.module('dev-server', [
   'angularjs-input-file'
-]);
+]).component('devServer', {
+  controller: ['$timeout', DevServerComponent],
+  template: `
+<form>
+  <label>no params</label>
+  <input-file data-ng-model="$ctrl.files"></input-file>
+</form>
+<form>
+  <label>multiple</label>
+  <input-file multiple></input-file>
+  <!-- Also accept multiple="true" -->
+</form>
+<form>
+  <label>ngChange - Default</label>
+  <input-file files-loaded="$ctrl.fileChanged"></input-file>
+</form>
+<form>
+  <label>ngChange - Base64</label>
+  <input-file files-loaded="$ctrl.fileChanged"
+              file-format="Base64">
+  </input-file>
+</form>
+<form>
+  <label>ngChange - Text</label>
+  <input-file files-loaded="$ctrl.fileChanged"
+              file-format="Text">
+  </input-file>
+</form>
+
+<div>
+  <h3>Result:</h3>
+  <pre>{{ $ctrl.files | json }}</pre>
+</div>
+`
+});
